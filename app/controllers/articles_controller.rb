c44f1ed7630ae011efb7cfc9cc636e387ddee21e
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-08-16 13:42:19 -0500

    edit and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def show
-      @article = Article.find(params[:id ])
+      @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -18,5 +18,22 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
