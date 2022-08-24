16b9447928cdb6d1f94f38a5f20d13326ef07fbd
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-08-23 19:40:05 -0500

    authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
+
   def new
     @user = User.new
   end
+
   def create 
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
     end
   end
+
   def edit
-    @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Your account was updated succesfully'
       redirect_to @user
@@ -26,12 +31,17 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
