9e63c25f5417b510b26eb93b6506465301f9f387
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-08-23 19:53:46 -0500

    controller methods as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in? #this makes this current user available for our views as well
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
