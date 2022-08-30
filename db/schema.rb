5af1ba7bae0c62155dd5eb4b4a4881359d59f50d
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-08-30 12:03:35 -0500

    many to many assotiation (article-category)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_162456) do
+ActiveRecord::Schema.define(version: 2022_08_30_164809) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
